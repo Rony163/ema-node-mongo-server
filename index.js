df0f45cc38ba53a5fda014144e8c033c1e1e74c6
Rony163 <minhajulislamrony43@gmail.com>
2021-10-25 18:22:42 +0600

    data load from database

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+// Middlewire
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gltfs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('online_shop');
+        const productsCollection = database.collection('products');
+
+        //GET PRODUCTS API
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            const count = await cursor.count();
+            res.send({
+                count,
+                products
+            });
+        })
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Ema John Simple Server');
+});
+
+app.listen(port, () => {
+    console.log('Server running on port:', port);
+});
